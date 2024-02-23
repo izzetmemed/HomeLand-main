e8c8332c94b7f4007b5b749983ee21a43aade608
izzet.memed <izzet.memmmedov1705@gmail.com>
2024-02-23 23:05:05 +0400

    end

--- src/MyComponents/FetchGetAll.jsx
+++ src/MyComponents/FetchGetAll.jsx
@@ -3,10 +3,15 @@ const FetchGetAll = (Controller) => {
   var token=sessionStorage.getItem("Resp");
   try {
     const baseUrl = process.env.REACT_APP_API_KEY;
+<<<<<<< HEAD
     const resp =  axios.get(`${baseUrl}${Controller}`,{
       headers: {
         "Authorization": `Bearer ${JSON.parse(token)}`,
       }});
+=======
+    console.log(baseUrl)
+    const resp =  axios.get(`${baseUrl}${Controller}`);
+>>>>>>> main
     return resp;
   } catch (error) {
     console.error("Error fetching data: ", );
